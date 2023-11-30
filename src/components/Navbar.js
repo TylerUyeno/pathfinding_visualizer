0dffe5eb04ed5a80d21adc551aa3227d0f5f4a37
uyeno24 <uyeno24@up.edu>
2023-11-30 14:19:27 -0800

    deployment changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,15 +9,6 @@ function Navbar({ pathfindingVisualizerRef }) {
       <Link to="/" className="headerName">Pathfinding Visualizer</Link>
       <div className="header-right">
         <div className="left-section">
-          <div className="dropdown">
-            {/* <button className="dropbtn"> Algorithms <i className="fa fa-caret-down"></i></button>
-            <div className="dropdown-content">
-              <a value="Dijkstra">Dijkstra</a>
-              <a value="A*">A*</a>
-              <a value="Depth First Search">Depth First Search</a>
-            </div> */}
-          </div>
-          {/* <button onClick={() => generateRandomMaze(pathfindingVisualizerRef.current)}>Generate Maze</button> */}
           <button class="button" onClick={() => window.location.reload(true)}> Clear Board </button>
         </div>
         <Link to="/about">About Us</Link>
