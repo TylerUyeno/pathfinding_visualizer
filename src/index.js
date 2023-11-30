a489e93602a062454861d0a94f21e3bf7397481a
uyeno24 <uyeno24@up.edu>
2023-11-30 13:25:00 -0800

    Upload Project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import '../src/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'font-awesome/css/font-awesome.min.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
